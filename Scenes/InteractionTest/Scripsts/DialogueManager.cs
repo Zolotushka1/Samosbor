a1636641af3220ea7961cb3f57712ff4de3bf7a1
Cospero <117189288+Cospero@users.noreply.github.com>
2022-11-07 07:52:32 +0300

    Система взаимодействия с окружением.
    Создан шаблон, по которому игрок будет взаимодействовать с объектами: двери, нпс, сундуки и тд.

---
+++ Scenes/InteractionTest/Scripsts/DialogueManager.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class DialogueManager : MonoBehaviour
+{
+    public NPC npc;
+
+    bool isTalking = false;
+
+    float distance;
+    float curResponseTracker = 0;
+
+    public GameObject player;
+
+    public GameObject dialogueUI;
+
+    public Text npcName;
+    public Text npcDialogueBox;
+    public Text playerResponse;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueUI.SetActive(false);
+    }
+
+    void OnMouseOver()
+    {
+         //минус, что  тригер срабатывает только когда мышка на неписе, в игре её можно скрыть и сделать по центру, но придется смотреть на непися.
+        if (distance <= 2.5f)
+        {
+            //триггер диалога
+            /*if (Input.GetKeyDown(KeyCode.E) && isTalking == false)
+            {
+                StartConversation();
+            }*/
+            if (Input.GetKeyDown(KeyCode.Escape) && isTalking == true)
+            {
+                EndDialogue();
+            }
+
+
+            if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+            {
+                curResponseTracker++;
+                if (curResponseTracker >= npc.playerDialogue.Length - 1)
+                {
+                    curResponseTracker = npc.playerDialogue.Length - 1;
+                }
+            }
+
+
+
+
+
+
+
+            else if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+            {
+                curResponseTracker--;
+                if (curResponseTracker < 0)
+                {
+                    curResponseTracker = 0;
+                }
+            }
+
+
+            if (curResponseTracker == 0 && npc.playerDialogue.Length >= 0)
+            {
+                playerResponse.text = npc.playerDialogue[0];
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    npcDialogueBox.text = npc.dialogue[1];
+                }
+            }
+            else if (curResponseTracker == 1 && npc.playerDialogue.Length >= 1)
+            {
+                playerResponse.text = npc.playerDialogue[1];
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    npcDialogueBox.text = npc.dialogue[2];
+                }
+            }
+            else if (curResponseTracker == 2 && npc.playerDialogue.Length >= 2)
+            {
+                playerResponse.text = npc.playerDialogue[2];
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    npcDialogueBox.text = npc.dialogue[3];
+                }
+            }
+        }
+
+
+    }
+
+    public void StartConversation() //начинает диалог
+    {
+        isTalking = true;
+        curResponseTracker = 0;
+        dialogueUI.SetActive(true);
+        npcName.text = npc.name;
+        npcDialogueBox.text = npc.dialogue[0];
+        
+
+    }
+
+
+
+    void EndDialogue() //заканчивает диалог
+    {
+        isTalking = false;
+        dialogueUI.SetActive(false);
+    }
+}
