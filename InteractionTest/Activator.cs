c9639ea08e5e502550d3bed7bf27a76b7f1f9340
cospero <cospero321@gmail.com>
2022-11-09 05:51:34 +0300

    диалоги с выборами через .ink(пока не работают кнопки выбора ответов)

---
+++ InteractionTest/Activator.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Activator : MonoBehaviour
+{
+	[SerializeField] string activationLine;
+
+	public UnityEvent OnActivatedButtonDown;
+
+
+	public void ActivateCursor()
+	{
+		UICursor.activeCursor.Activate(activationLine);
+	}
+	public void DeactivateCursor()
+	{
+		UICursor.activeCursor.Deactivate();
+	}
+	public void Activate()
+	{
+		OnActivatedButtonDown.Invoke();
+
+	}
+
+}
