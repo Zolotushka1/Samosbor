d2181fe7324aa9b217f52e81929c870558c39745
Lorled <101242303+Lorled@users.noreply.github.com>
2022-11-09 17:40:23 +0300

    Update Control.cs

--- Control.cs
+++ Control.cs
@@ -6,8 +6,6 @@ using UnityEngine.SceneManagement;
 public class Control : MonoBehaviour
 {
     private GameObject player; 
-    public int maxHealth = 100;
-    public static int Health;
     public float speed = 6; 
     public float _speed; 
     public int rotation = 250; 
@@ -19,7 +17,6 @@ public class Control : MonoBehaviour
     
     void Start()
     {
-        Health = maxHealth;
         IsDrawWeapon = false; 
         _speed = speed; 
         player = (GameObject)this.gameObject; 
@@ -28,40 +25,38 @@ public class Control : MonoBehaviour
     void Update()
     {
         gun();
-        die();
     }
     void gun()
     {
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.position += player.transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            speed = _speed / 2;
+            player.transform.position -= player.transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            speed = _speed * 2;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.position -= player.transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.position += player.transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            player.transform.position += player.transform.up * jump * Time.deltaTime;
+        }
         if (IsDrawWeapon == true) 
         {
-            speed = _speed; 
-            if (Input.GetKey(KeyCode.W)) 
-            {
-                player.transform.position += player.transform.forward * speed * Time.deltaTime; 
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                speed = _speed / 2; 
-                player.transform.position -= player.transform.forward * speed * Time.deltaTime; 
-            }
-            if (Input.GetKeyUp(KeyCode.S))
-            {
-                speed = _speed * 2; 
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                player.transform.position -= player.transform.right * speed * Time.deltaTime; 
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                player.transform.position += player.transform.right * speed * Time.deltaTime; 
-            }
-            if (Input.GetKey(KeyCode.Space))
-            {
-                player.transform.position += player.transform.up * jump * Time.deltaTime; 
-            }
-
-            if (Input.GetKey(KeyCode.Keypad1)) 
+            speed = _speed * 2; 
+            if (Input.GetKey(KeyCode.Tab))
             {
                 IsDrawWeapon = false; 
             }
@@ -69,40 +64,8 @@ public class Control : MonoBehaviour
         else if (IsDrawWeapon == false)
         {
             speed = _speed;
-            if (Input.GetKey(KeyCode.LeftShift)) 
-            {
-                speed = _speed * 2; 
-            }
-            if (Input.GetKeyUp(KeyCode.LeftShift)) 
-            {
-                speed = _speed; 
-            }
-            if (Input.GetKey(KeyCode.W)) 
-            {
-                player.transform.position += player.transform.forward * speed * Time.deltaTime; 
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                speed = _speed / 2;
-                player.transform.position -= player.transform.forward * speed * Time.deltaTime; 
-            }
-            if (Input.GetKeyUp(KeyCode.S))
-            {
-                speed = _speed; 
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                player.transform.position -= player.transform.right * speed * Time.deltaTime; 
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                player.transform.position += player.transform.right * speed * Time.deltaTime; 
-            }
-            if (Input.GetKey(KeyCode.Space))
-            {
-                player.transform.position += player.transform.up * jump * Time.deltaTime; 
-            }
-            if (Input.GetKey(KeyCode.Keypad2)) 
+            
+            if (Input.GetKey(KeyCode.Tab)) 
             {
                 IsDrawWeapon = true; 
             }
@@ -115,16 +78,5 @@ public class Control : MonoBehaviour
 
 
     }
-    public static void GetDamage(int DAMAge)
-    {
-        Health -= DAMAge;
-       
-    }
-    void die()
-    {
-        if (Health < 1)
-        {
-            SceneManager.LoadScene(0);
-        }
-    }
+
 }
