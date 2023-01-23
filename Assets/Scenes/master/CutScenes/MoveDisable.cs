b535879aa1661593ad8db114ae58029a9cff0be8
Evgenii Zolotarev <xca0@yandex.ru>
2023-01-23 23:36:24 +0300

    Cutscene move script

---
+++ Assets/Scenes/master/CutScenes/MoveDisable.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDisable : MonoBehaviour
+{
+    private CharacterController characterControl;
+    public bool enableController;
+    private GameObject basedCamera;
+    private HeadBob headBob;
+    private Player_MouseMove mouseMouve;
+    private Player_Move playerMove;
+    private GameObject cursorFind;
+    private Canvas cursor;
+
+    void Start()
+    {
+        basedCamera = GameObject.Find("Player_Camera");
+        cursorFind = GameObject.Find("Cursor");
+    }
+
+
+    void Update()
+    {
+        characterControl = GetComponent<CharacterController>();
+        mouseMouve = GetComponent<Player_MouseMove>();
+        playerMove = GetComponent<Player_Move>();
+        headBob = basedCamera.GetComponent<HeadBob>();
+        cursor = cursorFind.GetComponent<Canvas>();
+        if (enableController == true)
+        {
+            cursor.enabled = true;
+            characterControl.enabled = true;
+            headBob.enabled = true;
+            mouseMouve.enabled = true;
+            playerMove.enabled = true;
+
+        }
+        else if (enableController == false)
+        {
+            cursor.enabled = false;
+            characterControl.enabled = false;
+            headBob.enabled = false;
+            mouseMouve.enabled = false;
+            playerMove.enabled = false;
+        }
+    }
+}
