1486c9ab091092ea155b552c16d3a18a70b5a008
Evgenii Zolotarev <xca0@yandex.ru>
2023-02-04 16:25:18 +0300

    Fix

--- Assets/Scripts/Fokin/perem.cs
+++ Assets/Scripts/Fokin/perem.cs
@@ -7,21 +7,10 @@ public class perem : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
-    public float timeLi;
     public Vector3 dir;
-    void start()
-    {
-        StartCoroutine(nmat());
-    }
     void FixedUpdate ()
     {
-    
         transform.Translate(speed * dir * Time.deltaTime);
-    }
-    IEnumerator nmat()
-    {
-        yield return new WaitForSeconds(timeLi);
-        Destroy(gameObject);
     }
 
 
