4db35d7abc1ce18a26470b5e7ccbf474917fdd1d
Lorled <101242303+Lorled@users.noreply.github.com>
2023-03-01 21:40:40 +0300

    Update Zadacha.cs

--- Assets/Scripts/Fokin/Zadacha.cs
+++ Assets/Scripts/Fokin/Zadacha.cs
@@ -7,10 +7,13 @@ public class Zadacha : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI Missiontmp;
     [SerializeField] private string MissionText;
+    [SerializeField] private TextMeshProUGUI NameMissiontmp;
+    [SerializeField] private string NameMissionText;
     public Collider Player;
 
     void OnTriggerEnter(Collider Player)
     {
         Missiontmp.text = MissionText;
+        NameMissiontmp.text = NameMissionText;
     }
 }
