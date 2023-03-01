0a4547eec2c25f37e74c51ecaae1f40534be0a4f
Lorled <101242303+Lorled@users.noreply.github.com>
2023-03-02 01:00:00 +0300

    Add files via upload

---
+++ Assets/Scripts/Fokin/setActive.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setActive : MonoBehaviour
+{
+    public Collider Player;
+    [SerializeField] private GameObject obj;
+
+    void OnTriggerEnter(Collider Player)
+
+    {
+        if (obj == false)
+        {
+            obj.SetActive(true);
+            Destroy(gameObject);
+        }
+        if (obj == true)
+        {
+            obj.SetActive(false);
+            Destroy(gameObject);
+        }
+
+    }
+}
