b2f64d0644d08273feaf20798969ecb629eee7d7
Lorled <101242303+Lorled@users.noreply.github.com>
2023-03-25 13:44:33 +0300

    Add files via upload

---
+++ Assets/Scripts/Fokin/waa.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class waa : MonoBehaviour
+{
+    public float climbSpeed = 2.0f;
+    public float raycastDistance = 100f;
+    public LayerMask targetLayer;
+    public TextMeshProUGUI displayText;
+    public string customText = "Заданный текст";
+    public Behaviour componentToDisable;
+    private Rigidbody rb;
+    [SerializeField]private Camera mainCamera;
+    private bool isClimbing;
+    public float moveSpeed = 5.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+        displayText.enabled = false;
+    }
+
+    void Update()
+    {
+        RaycastToScreenCenter();
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+
+        if (!isClimbing)
+        {
+            componentToDisable.enabled = true;
+            rb.useGravity = true;
+        }
+        else
+        {
+            // Лезем по стене
+            Vector3 climbDirection = new Vector3(horizontal, vertical, 0);
+            transform.position += climbDirection * climbSpeed * Time.deltaTime;
+            componentToDisable.enabled = false;
+            rb.useGravity = false;
+           
+            
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isClimbing = false;
+        }
+        
+    }
+
+    void RaycastToScreenCenter()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, raycastDistance, targetLayer))
+        {
+
+            displayText.text = customText;
+            displayText.enabled = true;
+            
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                isClimbing = true;
+            }
+        }
+        else
+        {
+            displayText.enabled = false;
+        }
+        
+    }
+}
